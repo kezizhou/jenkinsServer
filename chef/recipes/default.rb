1379b25bbfdc13fb3f8dc07cc232f3d620f19580
kezizhou <kezizhou@gmail.com>
2019-12-06 08:15:50 -0500

    Adding cfts and chef

---
+++ chef/recipes/default.rb
@@ -0,0 +1,7 @@
+#
+# Cookbook:: jenkinsServer
+# Recipe:: default
+#
+# Copyright:: 2019, Keziah Zhou, All Rights Reserved.
+
+include_recipe 'jenkinsServer::install.rb'
---
+++ chef/recipes/install.rb
@@ -0,0 +1,25 @@
+#
+# Cookbook:: jenkinsServer
+# Recipe:: install
+#
+# Copyright:: 2019, Keziah Zhou, All Rights Reserved.
+
+execute 'Update yum' do
+    command "yum update -y"
+end
+
+execute 'Install Jenkins and requirements' do
+    command "<<-EOH
+        yum update -y
+        sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
+        yum install jenkins -y
+        yum install java-1.8.0 -y
+        yum remove java-1.7.0-openjdk -y
+        yum install git -y
+    EOH"
+end
+
+execute 'Start Jenkins' do
+    command "service jenkins start"
+end
+
